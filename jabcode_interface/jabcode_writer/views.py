b2440e0323fc7945fdd560eaf59d2647022d6d5f
Gabriel-Mac <gabrielmoraes@usp.br>
2023-04-13 16:52:08 -0300

    JabcodeGenerator implement.

--- jabcode_interface/jabcode_writer/views.py
+++ jabcode_interface/jabcode_writer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import subprocess
+import os
 
 # Create your views here.
 
@@ -10,8 +12,15 @@ def result(request):
 
 def process_textbox(request):
     if request.method == "POST":
+        
         textbox_content = request.POST['textbox']
-        print(textbox_content)
+        project_dir = os.path.dirname(os.path.abspath(__file__))
+
+        output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
+        output_file = os.path.join(output_dir, 'test.png')
+
+        subprocess.run(f'{os.path.join(project_dir, "jabcode_writer")} --input "{textbox_content}" --output "{output_file}"', shell=True)
+
         return render(request, 'result.html', {'my_string': textbox_content})
     else:
         pass
